95df1c2dd3756393eca7f0d21bf4960fd5a17981
Anjeet <anjeetpatel0306@gmail.com>
2024-01-29 12:46:34 +0530

    git ignore

--- LpuClasses/npm_package/app.js
+++ LpuClasses/npm_package/app.js
@@ -1,8 +1,13 @@
 const q = require('lodash');
 
+const a = require('./add');
+let p=a.add(2,4);
+console.log(p);
+
 const numbers = [1, 2, 3, 4, 5];
 
 const doubleNumbers = q.map(numbers, n=>n*2);
 
 console.log('original numbers: ',numbers);
 console.log('Doubled numbers: ',doubleNumbers);
+
